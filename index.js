d2a0a5cfbb10a10a46b379ecb1444accce1a1020
Lij623 <ejdg23@gmail.com>
2024-01-29 19:33:04 -0500

    Added button function

--- index.js
+++ index.js
@@ -2,21 +2,23 @@
 
 const word = document.getElementById('word');
 const changedWord = document.getElementById('changedWord');
-const usrGuess = document.getElementById('usrGuess');
+const result = document.getElementById('result');
 
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 chosen = [];
 
 let preChange; //variable for the word before the change
 let char; 
+let chgdWord;
 
 create();
 change(chosen);
 
+
 function create(){
 
         for(let i = 0; i < 10; i++) {
-            let index = Math.floor(Math.random() * letters.length) //floor to round down number so it can actually be used to search indexes in array
+            let index = Math.floor(Math.random() * letters.length); //floor to round down number so it can actually be used to search indexes in array
             char = letters[index]
             chosen.push(char); //pushes character into the chosen characters/word array
         }
@@ -26,16 +28,16 @@ function create(){
  }
 
 function change(array){
-    let shflChosen = []; //line 28- is used to shuffle the array and search to see if characters are already in our array or not
+    let shflChosen = [];// used to shuffle the array and search to see if characters are already in our array or not
     let usedIndexes = [];
 
-    let chgdWord;
+    
     let i = 0;
     while (i < array.length){
         let randomIndex = Math.floor(Math.random() * array.length);
 
         if (!usedIndexes.includes(randomIndex)){ //checks if its false that used index includes the random index
-            shflChosen.push(array[randomIndex]); // if its true that its false this gets pushed into the new array
+            shflChosen.push(array[randomIndex]);//this gets pushed into the new array
             usedIndexes.push(randomIndex);// adds this to used Indexes so that it can't be searched twice (or more)
             i++;//adds to stop the loop
         }
@@ -45,3 +47,19 @@ function change(array){
     
 }
 
+function convert(){
+    var usrGuess = document.getElementById('usrGuess').value;//grabs the users guess
+
+    let sorted = preChange.split("").sort().join(""); //splits the word letter by letter sorts and rejoins the word
+    let guess = chgdWord + usrGuess; //adds the guess to the changed word
+    guess = guess.split("").sort().join("");
+
+    if(sorted === guess){ //checks to see if words are the same 
+        result.textContent = 'Congratulations! That is the right answer!';
+    }
+    else{
+        result.textContent = 'Oops that is the wrong  answer! Please try again.';
+
+    }
+
+}
